d4d783662e831f6ac01c992c275dbb2eb0249339
sgt1796 <sgt1796@gmail.com>
2024-08-31 00:39:51 -0400

    add crawler

--- b4s_story_crawler.py
+++ b4s_story_crawler.py
@@ -8,27 +8,43 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 stories = soup.find_all('a', href=lambda x: x and x.startswith('/news/tonghua/'))
 ignore = ['童话故事', '中国童话故事']
-isIndex = False
 
-title = {}
+titles = {}
 index = {}
-for story in stories:
-    text = story.get_text().strip()
+page_number = 1
+while page_number == 1 or next_page is not None:
+    print('Page:', page_number)
+
+    isIndex = False
+    for story in stories:
+        text = story.get_text().strip()
+        if text in ignore:
+            print('Ignoring:', text)
+            continue
+        if text.isdigit():
+            isIndex = True
+            if text not in index:
+                index[text] = 'http://www.wpwx.cn' + story['href']
+        else:
+            if text not in titles:
+                titles[text] = 'http://www.wpwx.cn' + story['href']
     
-    if text in ignore:
-        print('Ignoring:', text)
-        continue
-    if text.isdigit():
-        isIndex = True
-        if text not in index:
-            index[text] = 'http://www.wpwx.cn' + story['href']
-    else:
-        if text not in title:
-            title[text] = 'http://www.wpwx.cn' + story['href']
-    
-    # exit after reading the indexes
-    if text.isdigit() and isIndex:
+        # exit after reading the indexes
+        if isIndex and not text.isdigit():
+            break
+
+        #print(text)
+        #print('http://www.wpwx.cn' + story['href'])
+
+    # print(index.values())
+    page_number += 1
+    next_page = index.get(str(page_number))
+
+    if next_page is None or page_number > 1000:
         break
 
-    print(text)
-    print('http://www.wpwx.cn' + story['href'])
+    response = requests.get(next_page)
+    response.encoding = 'gbk'
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    stories = soup.find_all('a', href=lambda x: x and x.startswith('/news/tonghua/'))
